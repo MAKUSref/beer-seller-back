b8d834d54f292626c26a29585a22b47e6f1759ae
MAKUSref <tomaszmakowski002731@gmail.com>
2025-05-30 16:59:26 +0200

    Add cors policy

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,22 @@
 import express from "express";
-import productRoute from './routes/product.route';
-import walletRouter from './routes/wallet.route';
+import productRoute from "./routes/product.route";
+import walletRouter from "./routes/wallet.route";
+import cors from "cors";
 
 const PORT = 3000;
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.use('/api/product', productRoute);
-app.use('/api/wallet', walletRouter)
+app.use("/api/product", productRoute);
+app.use("/api/wallet", walletRouter);
 
-app.get('/ping', (req, res) => {
+app.get("/ping", (_req, res) => {
   res.send("pong!!!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`)
-})
+  console.log(`Server running on port: ${PORT}`);
+});
